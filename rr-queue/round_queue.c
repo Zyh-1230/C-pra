05ee785b0a2d0808976965ee4ef3fd4764e59dc0
Zyh-1230 <chowyuhan@outlook.com>
2024-09-27 00:16:35 +0800

    sec

--- rr-queue/round_queue.c
+++ rr-queue/round_queue.c
@@ -10,68 +10,112 @@ typedef struct Queue
 
 }QUEUE;
 
-void init_queue(QUEUE*);//把定义的数组初始化
-bool en_queue(QUEUE*, int);//在数组中放入元素 形成队列
-bool full_queue(QUEUE*);
-void traverse(QUEUE*);
+void init_queue(QUEUE*, int);//把定义的数组初始化
+bool en_queue(QUEUE*, int, int);//在数组中放入元素 形成队列
+bool full_queue(QUEUE*, int);
+void traverse(QUEUE*, int);
+bool out_queue(QUEUE*, int*, int);
+bool empty_queue(QUEUE*);
 
 int main(void)
 {
     QUEUE Q;
     int val;
-     
-    init_queue(&Q);
-    
-    en_queue(&Q, 1);
-    en_queue(&Q, 2);
-    en_queue(&Q, 3);
-    en_queue(&Q, 4);
-    en_queue(&Q, 5);
-    en_queue(&Q, 6);
-    en_queue(&Q, 7);
-    en_queue(&Q, 8);
+    int len;
 
-    traverse(&Q);
+    printf("请输入你想要的队列元素的长度: ");
+    scanf("%d", &len);
+
+    init_queue(&Q, len);
+ 
+    en_queue(&Q, 1, len);
+    en_queue(&Q, 2, len);
+    en_queue(&Q, 3, len);
+    en_queue(&Q, 4, len);
+    en_queue(&Q, 5, len);
+    en_queue(&Q, 6, len);
+    en_queue(&Q, 7, len);
+    en_queue(&Q, 8, len);
+
+    if(empty_queue(&Q))
+        printf("你的队列为空！\n");
+    else
+        traverse(&Q, len);
+    
+    
+    if(out_queue(&Q, &val, len))
+    {
+        printf("出队成功!你队列中出队的元素是: %d\n", val);
+    }
+    else
+        printf("出队失败!你的队列为空！\n");
+        
+    traverse(&Q, len);
 
     return 0;
 }
 
-void init_queue(QUEUE* Pq)//让pBse指向7个连续的空间，front和rear相等且都为0
+void init_queue(QUEUE* Pq, int len)//让pBse指向7个连续的空间，front和rear相等且都为0
 {
-    Pq->pBase = (int*)malloc(sizeof(int) * 7);
+    Pq->pBase = (int*)malloc(sizeof(int) * len);
     Pq->front = 0;
     Pq->rear = 0;
 }
 
-bool full_queue(QUEUE* Pq)
+bool full_queue(QUEUE* Pq, int len)
 {
-    if ((Pq->rear+1) % 7 == Pq->front)
+    if ((Pq->rear+1) % len == Pq->front)
         return true;
     else
         return false;
 }
 
-bool en_queue(QUEUE* Pq, int val)
+bool en_queue(QUEUE* Pq, int val, int len)
 {
-    if (full_queue(Pq))
+    if (full_queue(Pq,len))
         return false;
     else
     {
         Pq->pBase[Pq->rear]= val;
-        Pq->rear = (Pq->rear+1) % 7;
+        Pq->rear = (Pq->rear+1) % len;
 
         return true;
     }
 
 }
 
-void traverse(QUEUE* Pq)
+void traverse(QUEUE* Pq, int len)
 {
     int i = Pq->front;
 
     while (i != Pq->rear)
     {
         printf("%d ",Pq->pBase[i]);//注意 不能写输出i
-        i = (i+1) % 7;
+        i = (i+1) % len;
     };
+    printf("\n");
+
+    return;
+}
+
+bool empty_queue(QUEUE* Pq)
+{
+    if (Pq->front == Pq->rear)
+        return true;
+    else
+        return false;
+}
+bool out_queue(QUEUE* Pq, int* pVal, int len)
+{
+    if (empty_queue(Pq))
+    {    
+        return false;
+    }
+    else
+    {
+        *pVal = Pq->pBase[Pq->front];
+        Pq->front = (Pq->front+1) % len;
+        
+    }
+    return true;
 }
