b75a4c7a35df66966aa980c2089a177be04fda09
Zyh-1230 <chowyuhan@outlook.com>
2024-10-04 18:18:12 +0800

    th

---
+++ Chain_binary_tree/Cmake/CMakeFiles/cbt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cbt.dir/Users/zhouyuhan/Desktop/c/Chain_binary_tree/CBTree.c.o"
+  "CMakeFiles/cbt.dir/Users/zhouyuhan/Desktop/c/Chain_binary_tree/CBTree.c.o.d"
+  "cbt"
+  "cbt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cbt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
